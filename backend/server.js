ddacd2009cf3ca0a844b725667628aeaaebfcd0b
Uttkarsh singh <140625490+uttkarsh123-shiv@users.noreply.github.com>
2025-02-12 15:23:02 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -13,7 +13,7 @@ const PORT = 5000;
 app.use(express.json());
 
 app.use(cors({
-  origin: 'https://udev-kohl.vercel.app/',  
+  origin: ["https://udev-kohl.vercel.app"],  
   methods: ['GET', 'POST', 'PUT', 'DELETE'],  
   credentials: true 
 }));
