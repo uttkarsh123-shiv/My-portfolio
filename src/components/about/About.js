426ac4cf4400cbf7eadfd40073c35440c8412746
Uttkarsh Singh <uttkarshsingh450@gmail.com>
2025-03-18 02:38:07 +0530

    Capitalise

--- src/components/about/About.js
+++ src/components/about/About.js
@@ -8,7 +8,7 @@ const About = () => {
   return (
     <section className='about section' id='about'>
       <h2 className='section__title'>About Me</h2>
-      <span className='section__subtitle'>My introduction</span>
+      <span className='section__subtitle'>My Introduction</span>
 
       <div className='about__container container grid'>
         <img src={AboutImg} alt='' className='about__img' />
--- src/components/qualification/Qualification.js
+++ src/components/qualification/Qualification.js
@@ -114,7 +114,7 @@ const Qualification = () => {
   return (
     <section className='qualification section' id='qualifications'>
       <h2 className='section__title'>Qualification</h2>
-      <span className='section__subtitle'>My personel journey</span>
+      <span className='section__subtitle'>My Personel Journey</span>
 
       <div className='qualification__container container'>
         <div className='qualification__tabs'>
