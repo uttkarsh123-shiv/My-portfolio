46453bd6df3697a07ff242830563c5bb2818632e
Uttkarsh Singh <uttkarshsingh450@gmail.com>
2025-03-31 16:08:51 +0530

    refactor(qualification): move data to separate file for maintainability
    This commit relocates the education, experience, and achievements data to a new file, qualificationData.js, to improve code organization and maintainability. The data is now imported into Qualification.js, reducing clutter and making it easier to manage.

---
+++ src/components/qualification/qualificationData.js
@@ -0,0 +1,75 @@
+export const educationData = [
+    {
+        title: "BTech CSE",
+        subtitle: "Maharaja Agrasen Institute of Technology, Delhi",
+        period: "2022-2026",
+        cgpa: "8.398 CGPA"
+    },
+    {
+        title: "Senior Secondary School",
+        subtitle: "Kendriya Vidyalaya, Delhi",
+        period: "2020-2022",
+        cgpa: "94% in CBSE Board"
+    },
+    {
+        title: "Secondary School",
+        subtitle: "D.A.V. Centenary Public School, Delhi",
+        period: "2014-2020",
+        cgpa: "93.33% in CBSE Board"
+    }
+];
+
+export const experienceData = [
+    {
+        title: "Full Stack Developer",
+        subtitle: "Truxie Startup",
+        period: "Nov 2024 - Present",
+        role: "Intern"
+    },
+    {
+        title: "Full Stack Developer",
+        subtitle: "CoderOne Company",
+        period: "Jun 2024 - Aug 2024",
+        role: "Intern"
+    },
+    {
+        title: "Full Stack Developer",
+        subtitle: "Startup Sphere Society",
+        period: "2022-2026",
+        role: "Self-Employed"
+    },
+    {
+        title: "Corporal, Social Head",
+        subtitle: "National Cadet Corps",
+        period: "2022-2025",
+        role: "Self-Employed"
+    },
+];
+
+export const achievementsData = [
+    {
+        title: "NCC Fest Organiser",
+        subtitle: "Kartavya'24 & 25",
+        period: "2024 & 2025",
+        description: "Organised 10+ events"
+    },
+    {
+        title: "Hackathon Finalists",
+        subtitle: "HackBlocks1.0",
+        period: "May 2024",
+        description: "Top 10 Finalists",
+    },
+    {
+        title: "Coding Competition Winner",
+        subtitle: "LogicMaze",
+        period: "March 2024",
+        description: "Bagged 3rd place"
+    },
+];
+
+// Define tabs data to make it more maintainable
+export const tabsData = [
+    { id: 1, name: "Education", icon: "uil-graduation-cap", data: educationData },
+    { id: 2, name: "Experience", icon: "uil-briefcase-alt", data: experienceData },
+    { id: 3, name: "Achievements", icon: "uil-trophy", data: achievementsData }
+];
